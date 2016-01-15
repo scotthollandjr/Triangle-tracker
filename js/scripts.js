b35fc06299dbc9d4bea7e6cf8241f15574ebcd3b
Scott Holland <scotthollandjr@gmail.com>
2016-01-14 21:18:23 -0800

    Add not a triangle functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 var triTrack = function(a, b, c) {
-  if ((a !== b) && (a !== c) && (b !== c)) {
+  if (( a + b <= c ) || ( a + c <= b ) || ( b + c <= a)) {
     return true;
-    alert("Scalene!")
+    alert("Not a triangle!");
+  } else if ((a !== b) && (a !== c) && (b !== c)) {
+    return true;
+    alert("Scalene!");
   } else if (( a === b ) && (a === c) && ( b === c )) {
     return true;
+    alert("Equilateral!");
   } else if (((a === b) && (b !== c)) || ((b === c) && (a !== b))) {
     return true;
+    alert("Isosceles!");
+  } else {
+    alert("Probably not a triangle!");
   }
 };
 
--- specs/specs.js
+++ specs/specs.js
@@ -1,4 +1,8 @@
 describe('triTrack', function() {
+  it("if the sum of any 2 sides is greater or = to the remaining side, not a triangle", function() {
+    expect(triTrack(20, 20, 50)).to.equal(true);
+  });
+
   it("a triangle with no equal sides should be a scalene", function() {
     expect(triTrack(15, 20, 10)).to.equal(true);
   });
