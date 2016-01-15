a39bab4f6eba675a123512b43fed46e90f4802b1
Scott Holland <scotthollandjr@gmail.com>
2016-01-14 21:40:21 -0800

    Fix isosceles functionality to include all possibilities

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,25 @@
-var triTrack = function(a, b, c) {
+function triTrack(a, b, c) {
   if (( a + b <= c ) || ( a + c <= b ) || ( b + c <= a)) {
-    return true;
     alert("Not a triangle!");
   } else if ((a !== b) && (a !== c) && (b !== c)) {
-    return true;
     alert("Scalene!");
   } else if (( a === b ) && (a === c) && ( b === c )) {
-    return true;
     alert("Equilateral!");
-  } else if (((a === b) && (b !== c)) || ((b === c) && (a !== b))) {
-    return true;
+  } else if (((a === b) && (b !== c)) || ((b === c) && (a !== b)) || ((a === c) && (a !== b))) {
     alert("Isosceles!");
   } else {
     alert("Probably not a triangle!");
   }
 };
 
+$(document).ready(function() {
+  $("form#triangle").submit(function(event) {
+    var a = parseInt($("input#side1").val());
+    var b = parseInt($("input#side2").val());
+    var c = parseInt($("input#side3").val());
 
+    triTrack(a, b, c);
 
-// else if ((a = b = c)) {
-//   return true;
-// }
+    event.preventDefault();
+  });
+});
