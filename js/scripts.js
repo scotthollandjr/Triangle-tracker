6ed2e6e3f83c5564be8056ae9d3c95d53a347b26
Scott Holland <scotthollandjr@gmail.com>
2016-01-14 20:46:27 -0800

    Add scalene functionality

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var triTrack = function(a, b, c) {
+  if ((a != b) && (a != c) && (b != c)) {
+    return true;
+    alert("Scalene!")
+  }
+}
+
+
+
+// else if ((a = b = c)) {
+//   return true;
+// }
--- specs/specs.js
+++ specs/specs.js
@@ -1,12 +1,11 @@
 describe('triTrack', function() {
-  it("", function() {
-    expect(triTrack()).to.equal();
+  it("a triangle with no equal sides should be a scalene", function() {
+    expect(triTrack(15, 20, 10)).to.equal(true);
   });
 });
 
-
-behaviour || input example || output example
-a + b <= c || a b c || not a triangle
-a = b = c || a b c || equilateral
-a = b != c || a b c || isosceles
-a != b != c || a b c || scalene
+// behaviour || input example || output example
+// a + b <= c || a20 b20 c50 || not a triangle
+// a = b = c || a20 b20 c20 || equilateral
+// a = b != c || a20 b20 c15 || isosceles
+// a != b != c || a15 b20 c10 || scalene
